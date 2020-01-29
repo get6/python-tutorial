6ef0c060e07e7df495e5c94a98066bdbe4e61fee
get6 <charles9306@icloud.com>
2020-01-29 13:03:12 +0900

    p67까지 봄

--- test2.py
+++ test2.py
@@ -1,3 +1,6 @@
+import math
+
+
 def p38():
     inputNum = int(input("숫자를 입력하세요 :"))
     result = 0
@@ -87,14 +90,19 @@ def sumEachNum(n):
 
 rangeNum = 15
 
-generatorExist = [sumEachNum(i) for i in range(rangeNum)]
-print(generatorExist)
 
-generatorNoExist = [i for i in range(rangeNum) if not (i in generatorExist)]
-print(generatorNoExist)
+def p53():
 
-print(sum(generatorNoExist))
-print()
+    generatorExist = [sumEachNum(i) for i in range(rangeNum)]
+    print(generatorExist)
+
+    generatorNoExist = [i for i in range(
+        rangeNum) if not (i in generatorExist)]
+    print(generatorNoExist)
+
+    print(sum(generatorNoExist))
+    print()
+
 
 def getSelfNumber(n):
     sumValue = n
@@ -103,11 +111,97 @@ def getSelfNumber(n):
         sumValue += n % 10
         print("나머지 {}".format(n % 10))
         # 소수점까지 계산해줌
-        # n = n / 10 
+        # n = n / 10
         n = n // 10
         print("나누기 {}".format(n))
         pass
     return sumValue
 
-print(getSelfNumber(rangeNum))
-# p44()
+
+# inputRange = int(input('숫자를 입력하세요 :'))
+
+
+def p54(inputRange):
+    first = 0
+    fibonacci = 1
+    nextfibonach = 0
+
+    while first < inputRange:
+        print(first, end=', ')
+        nextfibonach = first + fibonacci
+        first = fibonacci
+        fibonacci = nextfibonach
+
+
+def fibonacci(n):
+    a = [0, 1]
+    while n >= (a[-1] + a[-2]):
+        a.append(a[-1] + a[-2])
+    print(a)
+    pass
+
+
+# p54(inputRange)
+# print()
+# fibonacci(inputRange)
+
+def p57(num):
+    result = False
+    for i in range(1, num + 1):
+        if num % i == 0:
+            result = not result
+            if result == True:
+                print("{}번째 스위치 켜짐".format(i))
+            elif result == False:
+                print("{}번째 스위치 꺼짐".format(i))
+
+    if result == False:
+        print("no")
+    else:
+        print("yes")
+
+
+# p57(inputRange)
+
+def p60():
+    num = 120
+    sum = 0
+    for i in range(1, num+1):
+        print(math.sqrt(i))
+        print(math.sqrt(i) % 1)
+        if(math.sqrt(i) % 1 == 0):
+            print("석방된 죄수번호방 %d" % i)
+            sum += 1
+
+    print("석방된 죄수는 %d명입니다." % sum)
+
+# p60()
+
+
+def p62(a, b, c):
+    s = (a+b+c)/2
+    print(s)
+    heron = math.sqrt(s*(s-a)*(s-b)*(s-c))
+    print(heron)
+    r = heron/s
+    print(r)
+
+# p62(12,12,10)
+
+def p64(digit):
+    multiplier = pow(2,digit)
+    rSum = 0
+    # while multiplier > 0:
+    #     rSum += multiplier % 10
+    #     multiplier //= 10
+    # print(rSum)
+    multiplier = str(multiplier)
+    for i in multiplier:
+        print(i)
+        rSum += int(i)
+    print("2^1000의 각 자릿수의 합=", rSum)
+
+# p64(1000)
+print(eval('+'.join(str(2**1000))))
+print(eval('+'.join(str(pow(2,1000)))))
+print(sum(int(num) for num in str(pow(2,1000))))
