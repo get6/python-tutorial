b3a44e6b1aef3ad9c119c7f42bee9e7c0f32dbb5
get6 <charles9306@icloud.com>
2020-02-06 13:06:56 +0900

    P86 풀이

--- test4.py
+++ test4.py
@@ -1,3 +1,6 @@
+import re
+
+
 def comma(n):
     if n[0] == '-':
         # 음수 시작은 음수를 제외
@@ -11,22 +14,75 @@ def comma(n):
     else:
         return comma(n[:n.find('.')]) + n[n.find('.'):]
 
+
 def p79():
     print(comma('123456789.654321'))
 
-p79()
+# p79()
+
+
 amazonList = ['a1', 'a2', 'a3', 'a4', 'a5', 'b1', 'b2', 'b3', 'b4', 'b5']
 
 
 def p81_1(amazonList):
     # a4, a5를 지우면 b1 index가 앞으로 오기에 반복이 다 되지 않음
     amazonResult = []
-    index = amazonList.index('b1')   
+    index = amazonList.index('b1')
 
     for i in range(index):
         amazonResult.append(amazonList[i])
-        if index + i <= len(amazonList) -1:
+        if index + i <= len(amazonList) - 1:
             amazonResult.append(amazonList[index+i])
     print(amazonResult)
 
-p81_1(amazonList)
+
+def p81_2(amazonList):
+    amazonResult = []
+    index = amazonList.index('b1')
+
+    for i in range(index):
+        amazonResult.append(amazonList[i])
+        if index + i <= len(amazonList) - 1:
+            amazonResult.append(amazonList[index + i])
+            temp = index + i
+
+    print(amazonResult + amazonList[temp + 1:])
+
+
+def p81_3(amazonList):
+    index = amazonList.index('b1')
+
+    j = 0
+    for i in range(0, len(amazonList)-1, 2):
+        print(i)
+        amazonList.insert(i + 1, amazonList[index + j])
+        amazonList.pop(index + j + 1)
+        j += 1
+    print(amazonList)
+
+
+# p81_1(amazonList)
+# p81_2(amazonList)
+# p81_3(amazonList)
+
+def p87(score):
+    numList = []
+    domainIndexList = []
+    for i, val in enumerate(score):
+        if(val.isdecimal()): # 숫자인지 검사
+            numList.append(val)
+        if(val.isalpha()):
+            domainIndexList.append(i)
+    
+    # print(numList)
+    print(domainIndexList)
+
+    for i in domainIndexList:
+        print(i)
+        print(score[:i])
+
+    split = re.findall('[0-9]{1,2}[^0-9][*#]?', score)
+    print(split)
+
+
+p87('1S2D*3T')
