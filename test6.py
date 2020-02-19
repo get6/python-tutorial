a11f8f9b762cece9d329f152d6f3c407a07629c2
get6 <charles9306@icloud.com>
2020-02-19 12:58:21 +0900

    애니펑 예제

--- test6.py
+++ test6.py
@@ -1,21 +1,21 @@
 def funcLRU(cacheSize, cities):
-    if not(0 <= cacheSize <= 30):
-        print('에러:cache의 size를 벗어남')
+    if not (0 <= cacheSize <= 30):
+        print("에러:cache의 size를 벗어남")
         return
-    if not(0 < len(cities)<= 100000):
-        print('에러:도시의 개수를 벗어남')
+    if not (0 < len(cities) <= 100000):
+        print("에러:도시의 개수를 벗어남")
         return
     citiesLower = []
     for city in cities:
-        if not(0 < len(city) <=20):
-            print('에러:도시 이름은 20자 이하여야 합니다.')
+        if not (0 < len(city) <= 20):
+            print("에러:도시 이름은 20자 이하여야 합니다.")
             return
-        if not(city.isalpha()):
-            print('에러:도시 이름은 공백, 숫자, 특수문자 등이 없는 영문자여야 합니다.')
+        if not (city.isalpha()):
+            print("에러:도시 이름은 공백, 숫자, 특수문자 등이 없는 영문자여야 합니다.")
             return
         citiesLower.append(city.lower())
-    if(cacheSize == 0):
-        print(len(cities)*5)
+    if cacheSize == 0:
+        print(len(cities) * 5)
         return
     citiesCache = []
     runtime = 0
@@ -26,26 +26,160 @@ def funcLRU(cacheSize, cities):
             citiesCache.append(city)
         else:
             runtime += 5
-            if(len(citiesCache) == cacheSize):
+            if len(citiesCache) == cacheSize:
                 del citiesCache[0]
                 citiesCache.append(city)
             else:
                 citiesCache.append(city)
     print(runtime)
 
+
 def p102():
     testCases = [
-        [3, ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']],
-        [3, ['Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul']],
-        [2, ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'SanFrancisco', 'Seoul', 'Rome', 'Paris', 'Jeju', 'NewYork', 'Rome']],
-        [5, ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'SanFrancisco', 'Seoul', 'Rome', 'Paris', 'Jeju', 'NewYork', 'Rome']],
-        [2, ['Jeju', 'Pangyo', 'NewYork', 'newyork']],
-        [0, ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']],
-        [31, ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']],
-        [0, ['Jej*', 'Pangyo', 'Seoul', 'NewYork', 'LA']],
-        [0, ['Jejdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd']],
+        [
+            3,
+            [
+                "Jeju",
+                "Pangyo",
+                "Seoul",
+                "NewYork",
+                "LA",
+                "Jeju",
+                "Pangyo",
+                "Seoul",
+                "NewYork",
+                "LA",
+            ],
+        ],
+        [
+            3,
+            [
+                "Jeju",
+                "Pangyo",
+                "Seoul",
+                "Jeju",
+                "Pangyo",
+                "Seoul",
+                "Jeju",
+                "Pangyo",
+                "Seoul",
+            ],
+        ],
+        [
+            2,
+            [
+                "Jeju",
+                "Pangyo",
+                "Seoul",
+                "NewYork",
+                "LA",
+                "SanFrancisco",
+                "Seoul",
+                "Rome",
+                "Paris",
+                "Jeju",
+                "NewYork",
+                "Rome",
+            ],
+        ],
+        [
+            5,
+            [
+                "Jeju",
+                "Pangyo",
+                "Seoul",
+                "NewYork",
+                "LA",
+                "SanFrancisco",
+                "Seoul",
+                "Rome",
+                "Paris",
+                "Jeju",
+                "NewYork",
+                "Rome",
+            ],
+        ],
+        [2, ["Jeju", "Pangyo", "NewYork", "newyork"]],
+        [0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]],
+        [31, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]],
+        [0, ["Jej*", "Pangyo", "Seoul", "NewYork", "LA"]],
+        [0, ["Jejdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"]],
     ]
     for testCase in testCases:
         funcLRU(testCase[0], testCase[1])
 
-p102()
+
+# p102()
+
+
+def overturnList(argList):
+    argListTmp = list(zip(argList[0], argList[1], argList[2], argList[3], argList[4]))
+    return [list(argListTmp[i]) for i in range(len(argListTmp[0]))]
+
+
+def pung(argList):
+    argListTmp = [[] * 5 for i in range(0, 5)]
+    argListTmp = list(argList)
+    for i in range(0, 5):
+        if (len(set(argListTmp[i][0:5])) == 1) and not (0 in argListTmp[i][0:5]):
+            argListTmp[i][0:5] = [-1, -1, -1, -1, -1]
+        if (len(set(argListTmp[i][0:4])) == 1) and not (0 in argListTmp[i][0:4]):
+            argListTmp[i][0:4] = [-1, -1, -1, -1]
+        if (len(set(argListTmp[i][1:5])) == 1) and not (0 in argListTmp[i][1:5]):
+            argListTmp[i][1:5] = [-1, -1, -1, -1]
+        if (len(set(argListTmp[i][0:3])) == 1) and not (0 in argListTmp[i][0:3]):
+            argListTmp[i][0:3] = [-1, -1, -1]
+        if (len(set(argListTmp[i][1:4])) == 1) and not (0 in argListTmp[i][1:4]):
+            argListTmp[i][1:4] = [-1, -1, -1]
+        if (len(set(argListTmp[i][2:5])) == 1) and not (0 in argListTmp[i][2:5]):
+            argListTmp[i][2:5] = [-1, -1, -1]
+    return argListTmp
+
+
+def p114():
+    matrix = []
+    # 입력 형식
+    # for i in range(5):
+    #     matrix.append(list(map(int, input().split())))
+    matrix = [
+        [2, 4, 1, 2, 1],
+        [3, 4, 2, 3, 3],
+        [2, 4, 1, 2, 2],
+        [4, 4, 4, 1, 2],
+        [4, 2, 3, 3, 2],
+    ]
+
+    matrixTmp = [[] * 5 for i in range(0, 5)]
+
+    isValidCalc = True
+    while isValidCalc:
+        matrixTmp = list(matrix)
+        verticalMatrix = overturnList(matrixTmp)
+        verticalMatrix = pung(verticalMatrix)
+        verticalMatrix = overturnList(verticalMatrix)
+
+        matrix = pung(matrix)
+
+        isValidCalc = False
+        for i in range(0, 5):
+            for j in range(0, 5):
+                if (matrix[i][j] == -1) or (verticalMatrix[i][j] == -1):
+                    matrix[i][j] = -1
+                    isValidCalc = True
+
+        matrixTmp = list(matrix)
+        verticalMatrix = overturnList(matrixTmp)
+        for i in range(0, 5):
+            if -1 in verticalMatrix[i]:
+                cnt = verticalMatrix[i].count(-1)
+                for j in range(0, cnt):
+                    verticalMatrix[i].remove(-1)
+                    verticalMatrix[i].insert(0, 0)
+        matrix = overturnList(verticalMatrix)
+
+    print("최종")
+    for i in range(0, 5):
+        print(matrix[i])
+
+
+p114()
